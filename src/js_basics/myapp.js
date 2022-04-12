1cc6b3faaf95024bc2954107ca9fa92912e2a187
Audhil <smdaudhilbe@gmail.com>
2022-04-12 16:30:31 +0530

    add basics of JavaScript

---
+++ src/js_basics/myapp.js
@@ -0,0 +1,37 @@
+//  tutorial: https://www.youtube.com/watch?v=PlbupGCBV6w&list=PLsyeobzWxl7rrvgG7MLNIMSTzVCDZZcT4&index=1&ab_channel=Telusko
+console.log('jack and jill');
+let num = 4;
+console.log(num);
+let name = 'audhil';
+console.log(name);
+const pi = 3.14;
+console.log(pi);
+let num_float = 4.3;
+console.log(num_float);
+console.log(typeof num_float);
+console.log(typeof name);
+let hexNo = 0xff;
+let exponential = 1.4e13;
+let num_ = 100_00_000;
+let infinity = 5 / 0;
+let NegInfinity = -5 / 0;
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_VALUE);
+let bigInt = 100385340840242042n;
+// console.log(bigInt + 10); //  TypeError: Cannot mix BigInt and other types, use explicit conversions
+console.log(bigInt + 3n); //  100385340840242045n
+let name1 = 'mohammed';
+let name2 = 'audhil';
+let name3 = name1 + name2;
+let bool = 5 > 6;
+console.log(bool);
+console.log(typeof bool);
+let user = null;
+console.log(user); //  null
+console.log(typeof user); //  object
+let anotherUser;
+console.log(anotherUser); //  undefined - equal to NULL in Java?
+console.log(typeof anotherUser); //  undefined - equal to NULL in Java?
+let d = 9;
+console.log(d / 'audhil'); //  NaN - not a number
+console.log(typeof (d / 'audhil')); //  number
