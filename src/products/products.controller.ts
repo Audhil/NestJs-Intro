b2d534b9bb6bda54548cefdb96b73467f546f45a
Audhil <smdaudhilbe@gmail.com>
2022-04-14 19:55:09 +0530

    complete Basic CRUD apis in nestJs

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { Product } from './product.model';
 
@@ -24,5 +32,27 @@ export class ProductsController {
   @Get()
   getAllProducts(): Product[] {
     return this.productService.getProducts();
+  }
+
+  @Get(':id')
+  getProduct(@Param('id') prodId: string): Product {
+    return this.productService.getProductById(prodId);
+  }
+
+  @Patch(':id')
+  updateProduct(
+    @Param('id') prodId: string,
+    @Body('title') prodTitle: string,
+    @Body('description') prodDesc: string,
+    @Body('price') prodPrice: number,
+  ) {
+    this.productService.updateProduct(prodId, prodTitle, prodDesc, prodPrice);
+    return null;
+  }
+
+  @Delete(':id')
+  removeProduct(@Param('id') prodId: string) {
+    this.productService.deleteProduct(prodId);
+    return null;
   }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Product } from './product.model';
 
 @Injectable()
@@ -14,5 +14,38 @@ export class ProductsService {
 
   getProducts() {
     return [...this.products]; //  makes a new array with items of products[], ... is known as spread operator
+  }
+
+  findProduct(id: string): [Product, number] {
+    const index = this.products.findIndex((prod: Product) => prod.id === id);
+    const product = this.products[index];
+    if (!product)
+      throw new NotFoundException(`Could not find product with id ${id}`);
+    return [product, index];
+  }
+
+  getProductById(prodId: string) {
+    const product = this.findProduct(prodId)[0];
+    return { ...product };
+  }
+
+  updateProduct(
+    prodId: string,
+    prodTitle: string,
+    prodDesc: string,
+    prodPrice: number,
+  ) {
+    const [product, index] = this.findProduct(prodId);
+    const updatedProduct: Product = { ...product }; //  creating a copy of product
+    console.log(`updated prod: ${updatedProduct}`);
+    if (prodTitle) updatedProduct.title = prodTitle;
+    if (prodDesc) updatedProduct.description = prodDesc;
+    if (prodPrice) updatedProduct.price = prodPrice;
+    this.products[index] = updatedProduct;
+  }
+
+  deleteProduct(prodId: string) {
+    const index = this.findProduct(prodId)[1];
+    this.products.splice(index, 1);
   }
 }
